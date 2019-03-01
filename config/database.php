455136515fdba00662ebb3bb1b9fcffaeb235c12
Luuk Hamming <luuk.hamming@gmail.com>
2019-03-01 13:37:41 +0100

    readded pgsql

--- config/database.php
+++ config/database.php
@@ -21,7 +21,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
+    | Of course, examples of config     ng each database platform that is
     | supported by Laravel is shown below to make development simple.
     |
     |
@@ -47,6 +47,21 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+        ],
+
+        
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
         ],
 
     ],
