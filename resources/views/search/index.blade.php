3f2ebadd86119b291a988667f6e9e2887e5bb7e3
Luuk Hamming <luuk.hamming@gmail.com>
2019-03-07 15:54:10 +0100

    Styling and redirecting

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -12,55 +12,65 @@
 </head>
 
 <body>
-    <h1>Welcome to the search bank!</h1>
-@if(!empty($AllowAdd))
-    <a href="Search/create">Add an item to the dbd</a>
-@endif
-    
-    <table>
-            <tr>
-                <th>Naam Vacture | </th>
-                <th>Link naar de Vacture | </th>
-                <th>Soort | </th>
-                @if(!empty($AllowAdd))
-                <th>Delete</th>
-                @endif
-                
-            </tr>
-        @foreach ($Vactures as $Vacture)
-        <tr>
-            <td>{{$Vacture->Name}}</td>
-            <td>{{$Vacture->Link}}</td>
-            @switch($Vacture->Type)
-            @case("FSD")
-                <td>FullStack Developer</td>
-                @break
-            @case("FED")
-                <td>Front End Developer</td>
-                @break
+    <p class="title is-1 is-spaced">Welcome to the search bank!</p>
 
-            @case("BED")
-                <td>Back End Developer</td>
-                @break
-            @default
-                
-        @endswitch
-        @if(!empty($AllowAdd))
 
-        <td>
-            <form method="POST" action="Search/{{$Vacture->id}}">
-                @csrf
-                @method("DELETE")
-                <input type="submit" value="Delete from database" />
-            </form>
-        </td>
-        @endif
-        <td>
-        </tr>
-        <!--$Vacture->Type == "FSD"-->
 
-        @endforeach
-    </table>
+<div class="columns is-mobile is-centered">
+    <div class="column is-4" style="">
+                <table>
+                        <tr>
+                            <th>Naam Vacture | </th>
+                            <th>Link naar de Vacture | </th>
+                            <th>Soort | </th>
+                            @if(!empty($AllowAdd))
+                            <th>Delete</th>
+                            @endif
+                            
+                        </tr>
+                    @foreach ($Vactures as $Vacture)
+                    <tr>
+                        <td>{{$Vacture->Name}}</td>
+                        <td>{{$Vacture->Link}}</td>
+                        @switch($Vacture->Type)
+                        @case("FSD")
+                            <td>FullStack Developer</td>
+                            @break
+                        @case("FED")
+                            <td>Front End Developer</td>
+                            @break
+
+                        @case("BED")
+                            <td>Back End Developer</td>
+                            @break
+                        @default
+                            
+                    @endswitch
+                    @if(!empty($AllowAdd))
+
+                    <td>
+                        <form method="POST" action="Search/{{$Vacture->id}}">
+                            @csrf
+                            @method("DELETE")
+                            <input type="submit" value="Delete from database" />
+                        </form>
+                    </td>
+                    @endif
+                    <td>
+                    </tr>
+                    <!--$Vacture->Type == "FSD"-->
+
+                    @endforeach
+                </table>
+        </div>
+        <div class="column is-one-quarter is-centered" style="">
+            @if(!empty($AllowAdd))
+            <a href="Search/create">Add an item to the dbd</a>
+            @endif
+          </div>
+  </div>
+
+
 </body>
 </html>
 @endsection
