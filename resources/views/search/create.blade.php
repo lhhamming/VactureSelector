43b4e07fe08dc3f558bc39dd6ba13ccb695796d5
Luuk Hamming <luuk.hamming@gmail.com>
2019-03-07 16:25:22 +0100

    Added bulma

--- resources/views/search/create.blade.php
+++ resources/views/search/create.blade.php
@@ -11,20 +11,30 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <h1>Welcome to the create page</h1>
+    <p class="title is-1 is-spaced">Welcome to the create page</p>
 @if(!empty($errormessage))
     <h1 style="color: red;"> {{ $errormessage }}</h1>
 @endif
     <form action="/AddVac" method="POST">
         @csrf
-        <input name="naam" type="text" placeholder="Functie van de baan" required> <br> <br>
-        <input name="linknaar" type="text" placeholder="Link naar de vacature" required><br><br>
-        <select name="soort" required>
-            <option value="FED">Front end</option>
-            <option value="BED">Back end</option>
-            <option value="FSD">Full stack developer</option>
-        </select><br><br>
-        <input type="submit" value="Zet hem in de database!">
+        <div class="field">
+            <div class="control">
+                <input name="naam" class="input is-warning" type="text" style="width:200px;" placeholder="Functie van de baan" required> <br> <br>
+                <input name="linknaar" class="input is-warning" type="text" style="width:200px;" placeholder="Link naar de vacature" required><br><br>
+            </div>
+        </div>
+        <div class="field">
+            <div class="control">
+              <div class="select is-warning">
+                <select name="soort" required>
+                    <option value="FED">Front end</option>
+                    <option value="BED">Back end</option>
+                    <option value="FSD">Full stack developer</option>
+                </select><br><br>
+              </div>
+            </div>
+        </div>
+        <input class="button is-warning" type="submit" value="Zet hem in de database!">
     </form>
 </body>
 </html>
